519c4b05402cb4301addccb9dd3a0da6fa3e7089
Aidan Leary <learyaidan@gmail.com>
2025-04-01 17:02:53 -0400

    add exclusions to .gitignore for MDK-ARM toolchain

--- mcu/NUCLEO-F722ZE_test/STM32CubeMX/STM32F722ZETx/MX_Device/MX_Device.h
+++ mcu/NUCLEO-F722ZE_test/STM32CubeMX/STM32F722ZETx/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 01/04/2025 16:47:00
+ * Date        : 01/04/2025 16:58:56
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
