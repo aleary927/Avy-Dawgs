105d8c73976b79208b06e209ae9e351bf0062442
noah-sikorski <sikorskin1@gmail.com>
2025-04-23 16:03:33 -0600

    Updated guidance to accept buffers

--- beaconTracking/guidance.c
+++ beaconTracking/guidance.c
@@ -38,26 +38,26 @@ typedef struct
  * @return          one of STRAIGHT_AHEAD, TURN_LEFT, TURN_RIGHT, TURN_AROUND
  */
 
-Direction guidance_step(double Bpar_db, double Bperp_db, GuidanceState *st, const GuidanceParams *p)
+Direction guidance_step(const float *gbuffx, const float *gbuffy, unit32_t posx, unit32_t posy GuidanceState *st, const GuidanceParams *p)
 {
-    // 1) compute combined magnitude
-    double mag = sqrt(Bpar_db*Bpar_db + Bperp_db*Bperp_db);
 
-    // 2) detect drop vs. last magnitude
+    // 1) pull out the last sample from each buffer
+    uint32_t idx_x = (posx == 0) ? (GOERTZEL_BUF_SIZE - 1) : (posx - 1);
+    uint32_t idx_y = (posy == 0) ? (GOERTZEL_BUF_SIZE - 1) : (posy - 1);
+    float Bpar_db  = gbufx[idx_x];
+    float Bperp_db = gbufy[idx_y];
+
+    // 2) compute combined magnitude
+    float mag = sqrt(Bpar_db * Bpar_db + Bperp_db * Bperp_db);
+
+    // 3) detect drop vs. last magnitude
     bool drop = (mag < st->last_mag);
     st->last_mag = mag;
 
-    // 3) forward/reverse logic
+    // 4) forward/reverse logic
     if (!st->reverse_lock) 
     {
-        if (drop)
-        {
-            st->fwd_drops++;
-        }
-        else
-        {
-            st->fwd_drops = 0;
-        }
+        st->fwd_drops = drop ? (st->fwd_drops + 1) : 0;
         if (st->fwd_drops >= p->drop_steps)
         {
             st->reverse_lock = true;
@@ -67,20 +67,13 @@ Direction guidance_step(double Bpar_db, double Bperp_db, GuidanceState *st, cons
     } 
     else 
     {
-        if (st->cd_timer > 0) 
+        if (st->cd_timer > 0)
         {
             st->cd_timer--;
         } 
         else 
         {
-            if (drop)
-            {
-                st->rev_drops++;
-            }
-            else
-            {
-                st->rev_drops = 0;
-            }
+            st->rev_drops = drop ? (st->rev_drops + 1) : 0;
             if (st->rev_drops >= p->drop_steps) 
             {
                 st->reverse_lock = false;
@@ -89,21 +82,21 @@ Direction guidance_step(double Bpar_db, double Bperp_db, GuidanceState *st, cons
         }
     }
 
-    // 4) optionally invert components if in reverse mode
-    double Bpar = st->reverse_lock ? -Bpar_db : Bpar_db;
-    double Bperp= st->reverse_lock ? -Bperp_db: Bperp_db;
+    // 5) invert components if in reverse mode
+    float Bpar = st->reverse_lock ? -Bpar_db : Bpar_db;
+    float Bperp= st->reverse_lock ? -Bperp_db: Bperp_db;
 
-    // 5) decide suggestion
-    if (Bpar < 0.0) 
+    // 6) decide suggestion
+    if (Bpar < 0.0f) 
     {
         return TURN_AROUND;
     }
-    double ang = atan2(Bperp, Bpar);
-    if (fabs(ang) <= p->fwd_thresh)
+    float ang = atan2f(Bperp, Bpar);
+    if (fabsf(ang) <= p->fwd_thresh)
     {
         return STRAIGHT_AHEAD;
     } 
-    else if (ang > 0.0) 
+    else if (ang > 0.0f) 
     {
         return TURN_LEFT;
     } 
